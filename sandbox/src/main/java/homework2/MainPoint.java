460e54315658f05d997ecba9693ccbb3cd46d472
Evgeniy Maltsev <intel-inside@mail.ru>
2018-10-25 23:32:50 +0300

    Задание №2: Потренироваться использовать функции, классы, объекты и методы

--- sandbox/src/main/java/homework2/MainPoint.java
+++ sandbox/src/main/java/homework2/MainPoint.java
@@ -3,9 +3,9 @@ package homework2;
 public class MainPoint {
 
   public static void main(String[] args) {
-    Point pointA = new Point(1,2);
-    Point pointB = new Point(2,1);
+    Point p1 = new Point(1,2);
+    Point p2 = new Point(2,1);
 
-    System.out.println(Point.distance(pointA,pointB));
+    System.out.println(p1.distance(p2));
   }
 }
